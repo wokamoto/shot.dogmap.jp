5c02526b3bbabb36bbec4f6b3ce4f3538bad4a36
wokamoto <wokamoto1973@gmail.com>
2012-11-21 18:08:49 +0900

    Jetpack のバージョンアップ (ver.2.0.2)

--- wp-content/themes/PB2-LT/functions.php
+++ wp-content/themes/PB2-LT/functions.php
@@ -67,6 +67,7 @@ set_post_thumbnail_size( 160, 160, true );
 //**********************************************************************************
 // コンテンツに「いいね！」とか「Google+1」とかのボタンを追加
 //**********************************************************************************
+/*
 function add_content($content) {
 	if ( is_feed() )
 		return $content;
@@ -85,6 +86,7 @@ function add_content($content) {
 	return $content;
 }
 add_filter('the_content', 'add_content');
+*/
 
 //**********************************************************************************
 // フッターで読み込む JavaScript 等を指定する
@@ -110,12 +112,14 @@ function add_wp_footer() {
 		$js .= sprintf($js_tag, 'http://s.gravatar.com/js/gprofiles.js', '');
 	}
 
+/*
 	if (is_single() || is_page()) {
 		$js_inline .= "jQuery(function(){";
 		$js_inline .= "jQuery('a.tweet-this').unbind('click').click(function(){window.twttr=window.twttr||{};var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C>A) G=Math.round((C/2)-(A/2));window.twttr.shareWin=window.open('http://twitter.com/share','','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');E=F.createElement('script');E.src='http://platform.twitter.com/bookmarklets/share.js?v=1';F.getElementsByTagName('head')[0].appendChild(E);return false;});";
 		$js_inline .= "});";
 		$js .= '<script type="text/javascript" src="http://apis.google.com/js/plusone.js">{lang: \'ja\'}</script>' . "\n";
 	}
+*/
 
 	$js_inline .= "if (typeof wpOnload=='function') wpOnload();\n";
 
